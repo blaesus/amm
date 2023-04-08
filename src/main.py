49c4a12e06f5384220b9789ed5d248adb8364083
Andy Shu <shu@immux.com>
2023-04-08 21:59:03 +0800

    Probe: implement basic finding!

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import json
+import sqlite3
 from typing import List, TypedDict, Tuple
 
 
@@ -54,15 +55,34 @@ def is_model(name: str) -> bool:
 
 def probe(args: List[str]) -> None:
 
+
     search_root = "."
     if len(args) >= 1:
         search_root = args[0]
 
-    model_paths: List[str] = []
+
+    model_paths: List[(str, str)] = []
     for (base, subdirectories, filenames) in os.walk(search_root):
         for name in filenames:
             if is_model(name):
-                model_paths.append(os.path.join(base, name))
+                model_paths.append((base, name))
+
+
+    conn = sqlite3.connect('src/data/amm.db')
+    c = conn.cursor()
+    for (base, name) in model_paths:
+        print("\nfitting file {}".format(name))
+        c.execute("""
+        SELECT repos.id, repos.registry, repos.name, repos.latestDownload FROM file_records
+        JOIN checkpoints ON file_records.checkpointId = checkpoints.id
+        JOIN repos ON repos.id = checkpoints.repoId
+        WHERE file_records.filename LIKE '%{}%'
+        ORDER BY repos.latestDownload DESC;
+        """.format(name))
+        # print select result
+
+        print(c.fetchall())
+
 
     print(model_paths)
 
